2e0e52991b75d0833be5f67a8c56d3d078fad566
Jared Gabel <Fubukiyo@gmail.com>
2018-11-05 10:39:00 -0600

    First Commit, hopefully last?

---
+++ PrintConsoleGUI/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace PrintConsoleGUI
+{
+    partial class Printer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.print = new System.Windows.Forms.Button();
+            this.input1 = new System.Windows.Forms.TextBox();
+            this.printText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // print
+            // 
+            this.print.Location = new System.Drawing.Point(238, 54);
+            this.print.Name = "print";
+            this.print.Size = new System.Drawing.Size(75, 23);
+            this.print.TabIndex = 1;
+            this.print.Text = "Print";
+            this.print.UseVisualStyleBackColor = true;
+            this.print.Click += new System.EventHandler(this.print_Click);
+            // 
+            // input1
+            // 
+            this.input1.Location = new System.Drawing.Point(12, 25);
+            this.input1.Name = "input1";
+            this.input1.Size = new System.Drawing.Size(301, 20);
+            this.input1.TabIndex = 0;
+            // 
+            // printText
+            // 
+            this.printText.AutoSize = true;
+            this.printText.BackColor = System.Drawing.SystemColors.Control;
+            this.printText.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.printText.Location = new System.Drawing.Point(9, 9);
+            this.printText.Name = "printText";
+            this.printText.Size = new System.Drawing.Size(78, 13);
+            this.printText.TabIndex = 2;
+            this.printText.Text = "Enter your text:";
+            // 
+            // Printer
+            // 
+            this.AcceptButton = this.print;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(325, 89);
+            this.Controls.Add(this.printText);
+            this.Controls.Add(this.input1);
+            this.Controls.Add(this.print);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Printer";
+            this.Text = "Printer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button print;
+        private System.Windows.Forms.TextBox input1;
+        private System.Windows.Forms.Label printText;
+    }
+}
+
---
+++ PrintConsoleGUI/Form1.cs
@@ -0,0 +1,40 @@
+﻿/* Created by Jared Gabel, for use by whomever finds it useful.
+ * November 5th, 2018.
+ */
+
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace PrintConsoleGUI
+{
+    public partial class Printer : Form
+    {
+        
+        public Printer()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        string input = "";
+
+        private void print_Click(object sender, EventArgs e)
+        {
+            input = input1.Text;
+            string toPrint = "${" + Environment.NewLine + "^CT~~CD,~CC^~CT~" + Environment.NewLine + "^XA~TA000~JSN^LT0^MNW^MTT^PON^PMN^LH0,0^JMA^PR2,2~SD20^JUS^LRN^CI0^XZ" + Environment.NewLine + "^XA" + Environment.NewLine + "^MMT" + Environment.NewLine + "^PW600" + Environment.NewLine + "^LL0150" + Environment.NewLine + "^LS0" + Environment.NewLine + "^FT567,91^A0I,50,48^FH\\^" + input + "^FS" + Environment.NewLine + "^BY2,3,45^FT566,34^BCI,,N,N" + Environment.NewLine + "^FD>:" + input + "^FS" + Environment.NewLine + "^PQ2,0,1,Y^XZ" + Environment.NewLine +  "}$";
+
+            System.IO.File.WriteAllText("label.txt", toPrint);
+
+            System.Diagnostics.ProcessStartInfo psi = new System.Diagnostics.ProcessStartInfo("label.txt");
+            psi.Verb = "print";
+            Process.Start(psi);
+
+            input1.Text = "";
+        }
+    }
+}
