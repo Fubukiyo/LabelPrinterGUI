80af8bc944a3ab412cf1aba7f6334c8f909a2e5e
Jared Gabel <Fubukiyo@gmail.com>
2018-11-19 14:11:36 -0600

    Changed formatting

--- PrintConsoleGUI/Form1.cs
+++ PrintConsoleGUI/Form1.cs
@@ -26,7 +26,7 @@ namespace PrintConsoleGUI
         private void print_Click(object sender, EventArgs e)
         {
             input = input1.Text;
-            string toPrint = "${" + Environment.NewLine + "^CT~~CD,~CC^~CT~" + Environment.NewLine + "^XA~TA000~JSN^LT0^MNW^MTT^PON^PMN^LH0,0^JMA^PR2,2~SD20^JUS^LRN^CI0^XZ" + Environment.NewLine + "^XA" + Environment.NewLine + "^MMT" + Environment.NewLine + "^PW600" + Environment.NewLine + "^LL0150" + Environment.NewLine + "^LS0" + Environment.NewLine + "^FT567,91^A0I,50,48^FH\\^" + input + "^FS" + Environment.NewLine + "^BY2,3,45^FT566,34^BCI,,N,N" + Environment.NewLine + "^FD>:" + input + "^FS" + Environment.NewLine + "^PQ2,0,1,Y^XZ" + Environment.NewLine +  "}$";
+            string toPrint = "${" + Environment.NewLine + "^CT~~CD,~CC^~CT~" + Environment.NewLine + "^XA~TA000~JSN^LT0^MNW^MTT^PON^PMN^LH0,0^JMA^PR2,2~SD20^JUS^LRN^CI0^XZ" + Environment.NewLine + "^XA" + Environment.NewLine + "^MMT" + Environment.NewLine + "^PW600" + Environment.NewLine + "^LL0150" + Environment.NewLine + "^LS0" + Environment.NewLine + "^FT536,102^A0I,42,40^FH\\^FD" + input + "^FS" + Environment.NewLine + "^BY2,3,45^FT570,39^BCI,,N,N" + Environment.NewLine + "^FD>:" + input + "^FS" + Environment.NewLine + "^PQ1,0,1,Y^XZ" + Environment.NewLine +  "}$";
 
             System.IO.File.WriteAllText("label.txt", toPrint);
 
