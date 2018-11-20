fe667cbb06bae2f7b47e581967bd3ba759329030
Jared Gabel <Fubukiyo@gmail.com>
2018-11-20 11:14:38 -0600

    Added amount to print

--- PrintConsoleGUI/Form1.Designer.cs
+++ PrintConsoleGUI/Form1.Designer.cs
@@ -28,9 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Printer));
             this.print = new System.Windows.Forms.Button();
             this.input1 = new System.Windows.Forms.TextBox();
             this.printText = new System.Windows.Forms.Label();
+            this.printAmount = new System.Windows.Forms.Label();
+            this.pAmount = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // print
@@ -38,7 +41,7 @@
             this.print.Location = new System.Drawing.Point(238, 54);
             this.print.Name = "print";
             this.print.Size = new System.Drawing.Size(75, 23);
-            this.print.TabIndex = 1;
+            this.print.TabIndex = 2;
             this.print.Text = "Print";
             this.print.UseVisualStyleBackColor = true;
             this.print.Click += new System.EventHandler(this.print_Click);
@@ -61,6 +64,23 @@
             this.printText.TabIndex = 2;
             this.printText.Text = "Enter your text:";
             // 
+            // printAmount
+            // 
+            this.printAmount.AutoSize = true;
+            this.printAmount.Location = new System.Drawing.Point(12, 59);
+            this.printAmount.Name = "printAmount";
+            this.printAmount.Size = new System.Drawing.Size(46, 13);
+            this.printAmount.TabIndex = 3;
+            this.printAmount.Text = "Amount:";
+            // 
+            // pAmount
+            // 
+            this.pAmount.Location = new System.Drawing.Point(64, 56);
+            this.pAmount.Name = "pAmount";
+            this.pAmount.Size = new System.Drawing.Size(28, 20);
+            this.pAmount.TabIndex = 1;
+            this.pAmount.Text = "2";
+            // 
             // Printer
             // 
             this.AcceptButton = this.print;
@@ -68,11 +88,14 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(325, 89);
+            this.Controls.Add(this.pAmount);
+            this.Controls.Add(this.printAmount);
             this.Controls.Add(this.printText);
             this.Controls.Add(this.input1);
             this.Controls.Add(this.print);
             this.ForeColor = System.Drawing.Color.Black;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Printer";
             this.Text = "Printer";
@@ -87,6 +110,8 @@
         private System.Windows.Forms.Button print;
         private System.Windows.Forms.TextBox input1;
         private System.Windows.Forms.Label printText;
+        private System.Windows.Forms.Label printAmount;
+        private System.Windows.Forms.TextBox pAmount;
     }
 }
 
